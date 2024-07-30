5d5f5f3b7c3fd1f5ffa2244acd337903015f9ba3
Mahendran P <158137779+Mahendra8940@users.noreply.github.com>
2024-07-30 08:41:09 +0530

    Add files via upload

---
+++ predictive_analytics.py
@@ -0,0 +1,183 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+
+import matplotlib.pyplot as plt
+
+data = pd.read_csv(r"c:\Users\CHINNAMUTHU\Downloads\insurance.csv")
+print(data.head(15))
+
+############################################   Handling Missing Values   ###################################################
+#check for how many missing values
+count_nan = data.isnull().sum()
+print(count_nan[count_nan>0])
+
+#fill in missing values
+data['bmi'].fillna(data['bmi'].mean(), inplace=True)
+
+#check if there are any missing values left
+count_nan = data.isnull().sum()
+print(count_nan[count_nan>0])
+
+
+############################################  Convert Categorical Data to Numbers  ##############################################
+#create array for label encodoing (sklearn)
+sex = data.iloc[:,1:2].values
+smoker = data.iloc[:,4:5].values
+
+#label encoder = le
+
+## le for sex
+le = LabelEncoder()
+sex[:,0] = le.fit_transform(sex[:,0])
+sex = pd.DataFrame(sex)
+sex.columns = ['sex']
+le_sex_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
+print("Sklearn label encoder results for sex:")
+print(le_sex_mapping)
+print(sex[:10])
+
+## le for smoker
+le = LabelEncoder()
+smoker[:,0] = le.fit_transform(smoker[:,0])
+smoker = pd.DataFrame(smoker)
+smoker.columns = ['smoker']
+le_smoker_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
+print("Sklearn label encoder results for smoker:")
+print(le_smoker_mapping)
+print(smoker[:10])
+
+#create ndarray for one hot encodoing (sklearn)
+region = data.iloc[:,5:6].values #ndarray
+
+## ohe for region
+ohe = OneHotEncoder() 
+
+region = ohe.fit_transform(region).toarray()
+region = pd.DataFrame(region)
+region.columns = ['northeast', 'northwest', 'southeast', 'southwest']
+print("Sklearn one hot encoder results for region:")  
+print(region[:10])
+
+############################################  Dividing the data to test and train  ##############################################
+
+#putting the data together:
+
+##take the numerical data from the original data
+X_num = data[['age', 'bmi', 'children']].copy()
+
+##take the encoded data and add to numerical data
+X_final = pd.concat([X_num, region, sex, smoker], axis = 1)
+
+#define y as being the "charges column" from the original dataset
+y_final = data[['charges']].copy()
+
+#Test train split
+X_train, X_test, y_train, y_test = train_test_split(X_final, y_final, test_size = 0.33, random_state = 0 )
+#X_train, X_test, y_train, y_test = train_test_split(data[['age']], y_final, test_size = 0.33, random_state = 0 )
+
+############################################ Feature Scaling ##############################################
+
+###normalized scaler (fit transform on train, fit only on test)
+#n_scaler = MinMaxScaler()
+#X_train = n_scaler.fit_transform(X_train.astype(np.float))
+#X_test= n_scaler.transform(X_test.astype(np.float))
+
+
+#standard scaler (fit transform on train, fit only on test)
+s_scaler = StandardScaler()
+X_train = s_scaler.fit_transform(X_train.astype(float))
+X_test= s_scaler.transform(X_test.astype(float))
+
+
+############################################  Linear Regression ##############################################
+
+lr = LinearRegression().fit(X_train,y_train)
+y_train_pred = lr.predict(X_train)
+y_test_pred = lr.predict(X_test)
+
+#print score
+print("lr.coef_: {}".format(lr.coef_))
+print("lr.intercept_: {}".format(lr.intercept_))
+print('lr train score %.3f, lr test score: %.3f' % (
+lr.score(X_train,y_train),
+lr.score(X_test, y_test)))
+
+############################################  Polynomial Regression  ##############################################
+
+poly = PolynomialFeatures (degree = 2)
+X_poly = poly.fit_transform(X_final)
+
+X_train,X_test,y_train,y_test = train_test_split(X_poly,y_final, test_size = 0.33, random_state = 0)
+
+#standard scaler (fit transform on train, fit only on test)
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train.astype(float))
+X_test= sc.transform(X_test.astype(float))
+
+#fit model
+poly_lr = LinearRegression().fit(X_train,y_train)
+
+y_train_pred = poly_lr.predict(X_train)
+y_test_pred = poly_lr.predict(X_test)
+
+#print score
+print('poly train score %.3f, poly test score: %.3f' % (
+poly_lr.score(X_train,y_train),
+poly_lr.score(X_test, y_test)))
+
+############################################  Support Vector Regression  ##############################################
+
+svr = SVR(kernel='linear', C = 300)
+
+#test train split
+X_train, X_test, y_train, y_test = train_test_split(X_final, y_final, test_size = 0.33, random_state = 0 )
+
+#standard scaler (fit transform on train, fit only on test)
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train.astype(float))
+X_test= sc.transform(X_test.astype(float))
+
+#fit model
+svr = svr.fit(X_train,y_train.values.ravel())
+y_train_pred = svr.predict(X_train)
+y_test_pred = svr.predict(X_test)
+
+#print score
+print('svr train score %.3f, svr test score: %.3f' % (
+svr.score(X_train,y_train),
+svr.score(X_test, y_test)))
+
+############################################  Decision Tree  ##############################################
+
+dt = DecisionTreeRegressor(random_state=0)
+
+#test train split
+X_train, X_test, y_train, y_test = train_test_split(X_final, y_final, test_size = 0.33, random_state = 0 )
+
+#standard scaler (fit transform on train, fit only on test)
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train.astype(float))
+X_test= sc.transform(X_test.astype(float))
+
+#fit model
+dt = dt.fit(X_train,y_train.values.ravel())
+y_train_pred = dt.predict(X_train)
+y_test_pred = dt.predict(X_test)
+
+#print score
+print('dt train score %.3f, dt test score: %.3f' % (
+dt.score(X_train,y_train),
+dt.score(X_test, y_test)))
